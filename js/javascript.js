7e86eacd1110b1ab2ec6c4a00613a87af5382ea7
Sylvana04 <cheriossinobasimh@gmail.com>
2024-11-19 21:15:23 -0600

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const nav = document.querySelector('nav ul');
+    const menuIcon = document.querySelector('.menu-icon');
+    nav.classList.toggle('active'); // Muestra o esconde el menú
+    menuIcon.classList.toggle('open'); // Cambia el ícono de hamburguesa a "X"
+}
